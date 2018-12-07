c3c4bf6eb2ce18fc25247f37ccfd0aca8846cf69
truyenho95 <truyenho.webdev@gmail.com>
2018-12-08 05:36:03 +0700

    Add enter value of elements in array feature

--- src/FindMinValue.java
+++ src/FindMinValue.java
@@ -1,8 +1,24 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class FindMinValue {
   public static void main(String[] args) {
-    int[] arr = {4, 12, 7, 8, 1, 6, 9};
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Please, enter size of array: ");
+    int sizeOfArray = scanner.nextInt();
+    int[] arr = new int[sizeOfArray];
+
+    System.out.println("Please, enter value of elements in array:");
+    for (int i = 0; i < sizeOfArray; i++) {
+      System.out.println("Elements["+i+"]");
+      arr[i] = scanner.nextInt();
+    }
+
+    System.out.println("Your array:");
+    System.out.println(Arrays.toString(arr));
+
     int index = minValue(arr);
-    System.out.println("The smallest element in the array is: " + arr[index]);
+    System.out.println("The smallest value of elements in the array is: " + arr[index]);
   }
 
   public static int minValue(int[] array) {
