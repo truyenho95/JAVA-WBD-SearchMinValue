f992850541059489333bf9852d7115e031822cbd
truyenho95 <truyenho.webdev@gmail.com>
2018-12-08 05:26:48 +0700

    Change name class from SearchMinValue to FindMinValue

--- src/FindMinValue.java
+++ src/FindMinValue.java
@@ -1,4 +1,4 @@
-public class SearchMinValue {
+public class FindMinValue {
   public static void main(String[] args) {
     int[] arr = {4, 12, 7, 8, 1, 6, 9};
     int index = minValue(arr);
@@ -6,7 +6,6 @@ public class SearchMinValue {
   }
 
   public static int minValue(int[] array) {
-
     int min = array[0];
     int index = 0;
 
